e54f5145b246f1c000e2b4d6db3062363f20a1ec
DrEGZo <32690381+DrEGZo@users.noreply.github.com>
2018-03-13 16:23:27 +0100

    Update vom 12.03.2018

---
+++ de-de/blog/blogentry/index.js
@@ -0,0 +1,17 @@
+$(function () {
+  /*var comments = document.querySelectorAll('.comment-content');
+  for (var i = 0; i < comments.length; i++) {
+    var content = undefined;
+    if (document.querySelectorAll('.comment-content > .comment-replies')[i] != undefined) {
+      content = document.querySelectorAll('.comment-content > .comment-replies')[i].innerHTML;
+    }
+    console.log(content);
+    if (content == '') {
+      try {
+        document.querySelectorAll('.comment-content > h6')[i].style.display = 'none';
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }*/
+});
--- server.js
+++ server.js
@@ -13,30 +13,42 @@ var contentTypes = {
 }
 
 http.createServer((req, res)=>{
-  if (req.url == "/" || req.url == "/de-de/") {
+  if (req.url == "" || req.url == "/" || req.url == "/de-de/" || req.url == "/de-de") {
     res.writeHead(302,  {Location: "http://" + req.headers.host + "/de-de/index.html"});
     res.end();
-  } else if (req.url == "/en-us/") {
+  } else if (req.url == "/en-us/" || req.url == "/en-us") {
     res.writeHead(302,  {Location: "http://" + req.headers.host + "/en-us/index.html"});
     res.end();
   } else {
     fs.readFile("."+req.url, (err,data)=>{
-      if (err) {
-        fs.readFile("."+req.url+"index.html", (err,data)=>{
-          if (err) {
-            res.writeHead(404);
-            res.end("ERROR 404: FILE NOT FOUND");
-          } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-          }
-        });
-      } else {
+      if (!err) {
         var parts = req.url.split("/");
         var extension = parts[parts.length-1].split(".")[1];
         res.writeHead(200, {'Content-Type': contentTypes[extension]});
         res.end(data);
+      } else {
+        var parts = req.url.split("/");
+        var file = "index.html";
+        if (parts[parts.length - 1] != "") {
+          file = "/" + file;
+        }
+        fs.readFile("."+req.url+file, (err,data)=>{
+          if (!err) {
+            res.writeHead(302,  {Location: "http://" + req.headers.host + req.url + file});
+            res.end();
+          } else {
+            res.writeHead(404);
+            res.end("ERROR 404: FILE NOT FOUND");
+          }
+        });
       }
     });
   }
 }).listen(port);
+
+/*
+Versuch, Datei zu öffnen
+Wenn err: de-de und en-us
+Wenn err: +index.html bzw. +/index.html
+Wenn err: 404
+*/
